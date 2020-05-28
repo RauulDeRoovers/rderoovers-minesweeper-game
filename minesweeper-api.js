43f8f46c06b6a863d8ba511708e7ca7161a382f7
Raúl De Roovers <nari77@gmail.com>
2020-05-28 18:33:15 -0300

    ¡First draft!

---
+++ minesweeper-api.js
@@ -0,0 +1,24 @@
+var minesweeperService = function($http) {
+	return {
+		createNewGame: function(rows, cols, mines) {
+			let data = JSON.stringify({
+				rowCount: rows,
+				columnCount: cols,
+				mineCount: mines
+			});
+			//return $http.post('http://localhost:8080/create', data);
+			return $http.post('https://minesweeper-rest-service.herokuapp.com/create', data);
+		},
+		updateGame: function(id, index) {
+			let data = JSON.stringify({
+				id: id,
+				square: index
+			});
+			//return $http.patch('http://localhost:8080/update', data);
+			return $http.patch('https://minesweeper-rest-service.herokuapp.com/update', data);
+		}
+	}
+};
+
+var app = angular.module('minesweeperServiceModule', []);
+app.service('MinesweeperService', minesweeperService);
