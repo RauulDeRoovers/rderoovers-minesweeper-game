fa7dd88f724603077e0171852bbbe443f196f639
Raúl De Roovers <nari77@gmail.com>
2020-05-28 19:01:48 -0300

    Attempting to create client

--- app.js
+++ app.js
@@ -1,52 +1,10 @@
-(function(angular) {
-  'use strict';
-angular.module('ngAppMinesweeper', ['minesweeperServiceModule']).controller('ngAppMinesweeperController', function($scope, $http, MinesweeperService) {
-  $scope.rows = 8;
-  $scope.cols = 8;
-  $scope.mines = 8;
-  $scope.status = 'Playing...';
-  $scope.game = undefined;
-  $scope.init = function() {$scope.newGame()};
-	$scope.newGame = function() {
-		MinesweeperService.createNewGame($scope.rows, $scope.cols, $scope.mines).then( 
-			function(response) { 
-				$scope.game = response.data;
-				$scope.status = 'Playing...';
-			}, 
-			function(response) {
-				if (response.status === -1) {
-					alert('Destination unreachable.');
-				}
-				else {
-					alert('Unknown error.');
-				}
-			} 
-		);
-	};
-	$scope.squareClick = function(index) {
-		MinesweeperService.updateGame($scope.game.id, index).then( 
-			function(response) { 
-				$scope.game = response.data;
-				if ($scope.game.finished) {
-					$scope.status = $scope.game.victory ? 'You win!' : 'You lose!';
-				}
-			}, 
-			function(response) {
-				if (response.status === -1) {
-					alert('Destination unreachable.');
-				}
-				else if (response.status === 405) {
-					alert('Game is over, please click "New Game!" to start a new game.');
-				}
-				else if (response.status === 400) {
-					alert('Invalid parameters, please do not tamper with arguments.');
-				}
-				else {
-					alert('Unknown error.');
-				}
-			}
-		);
-	}
-	$scope.init();
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
-})(window.angular);
+
+app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
+app.listen(process.env.PORT || 3000);
